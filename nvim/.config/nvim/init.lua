7d5f486954b991c5fcbe75cdafd13e78e2e7a8b1
Vadim <vadimpasinicev@gmail.com>
2025-11-06 08:40:11 +0000

    deleted vauge in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,12 @@
-require("config.lazy")
-vim.cmd.colorscheme("vague")
+--require("config.lazy")
+
+-- Defualt config
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.mouse = ""
+
+-- Disable background
+vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
